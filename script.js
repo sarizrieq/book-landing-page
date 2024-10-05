c43f3d23ca230dbb47397e84eb6cb4616b41d5fb
sarizrieq <SariZrieqBook@gmail.com>
2024-10-05 12:52:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ const mobileMenu = document.getElementById('mobile-menu');
 // Toggle both the mobile menu and the hamburger icon itself
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
+    hamburger.classList.toggle('active'); // Add this line to toggle the active state on the hamburger
 });
